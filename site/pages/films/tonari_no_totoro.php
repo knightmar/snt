2dbaac512db2e9bba3b4dd381ecc60d0400c3fc4
knightmar <74785484+knightmar@users.noreply.github.com>
2022-01-11 15:54:06 +0100

    add an image and a film

--- site/pages/films/tonari_no_totoro.php
+++ site/pages/films/tonari_no_totoro.php
@@ -5,9 +5,29 @@
     <title>Tonari no Totoro</title>
     <link href="/site/style/menu.css" rel="stylesheet" type="text/css">
     <link href="/site/style/general.css" rel="stylesheet" type="text/css">
+    <link href="/site/style/films.css" rel="stylesheet" type="text/css">
+
 
 </head>
 <body>
-<?php include './site/pages/utils/menu.html'?>
+<?php include './site/pages/utils/menu.html' ?>
+
+<h1 class="title">Mon voisin Totoro (1988)</h1>
+<img id="img_logo_totoro" src="/site/images/tonari_no_totoro.jpg" alt="Logo officiel">
+<i id="img_logo_totoro_desc">
+    Logo officiel Tonari no totoro
+</i>
+<section class="content">
+    <h3>Histoire :</h3>
+    <p>Mon voisin Totoro (となりのトトロ, Tonari no Totoro) est un film d'animation japonais réalisé par <a
+                href="/site/pages/perso/miyazaki.php">Hayao Miyazaki</a> et
+        produit par le studio Ghibli, sorti au Japon le 16 avril 1988.
+
+        Il est récompensé la même année par le prix Noburō Ōfuji et le Prix Mainichi du meilleur film.
+
+        Ce film a été diffusé pour la première fois en France lors du Festival du cinéma pour enfant de Corbeil-Essonnes
+        en 1992. Il fut ensuite disponible dans toute la France au cinéma le 8 décembre 19992, avant une ressortie en
+        2002. </p>
+</section>
 </body>
 </html>
